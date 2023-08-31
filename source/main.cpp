863d1f7be53e0c4c846c6e26c265f4c2aa140832
RepubblicaTech <76620155+RepubblicaTech@users.noreply.github.com>
2023-08-31 21:10:14 +0200

    Code comments

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,7 @@
 #include <3ds.h>
 #include <stdio.h>
 
+// init some logicGates menu vars
 int menubar = 0;
 int menucount = 0;
 char* menuStuff[] = {"NOT gate", "AND gate", "NAND gate"};
@@ -8,7 +9,7 @@ char* menuStuff[] = {"NOT gate", "AND gate", "NAND gate"};
 int main(int argc, char **argv) {
     // Preparation stuff
     for (auto i: menuStuff) {
-        menucount++;
+        menucount++;            // get items quantity from the menu array
     }
 
     gfxInitDefault();
@@ -17,7 +18,7 @@ int main(int argc, char **argv) {
     consoleInit(GFX_TOP, &topScreen);
     // Stuff to render on top screen
     printf("\x1b[10;5HLogisim3D Alpha codename 'Citrus' build 23\n");
-    printf("\n  Use the L and R triggers to change the gates below");
+    printf("\nUse the L and R triggers to change the gates below");
 
     consoleInit(GFX_BOTTOM, &bottomScreen);
     // Stuff to render on bottom screen
@@ -28,15 +29,30 @@ int main(int argc, char **argv) {
         u32 kDown = hidKeysDown();
 
         if (kDown & KEY_START) break;
+
+        // scroll through the logicGates menu
         if (kDown & KEY_L) menubar--;
         if (kDown & KEY_R) menubar++;
 
-        consoleClear();
+        consoleClear();             // Clears the bottom screen to avoid overlapping text
 
+        /*  Example
+
+            NAND GATE
+
+            After scrolling:
+
+            AND GATEE <--- This E was from before scrolling
+            consoleClear() should do the job for now
+            (we may need to change it ASAP, it could be an issue for future stuff)
+        */
+
+
+        // this is to get the "infinite" scroll
         if (menubar < 0) { menubar = menucount; }
         if (menubar >= menucount) { menubar = 0; }
 
-        printf("\n\x1b[2;0H%s\n", menuStuff[menubar]);
+        printf("\n\x1b[2;0H%s\n", menuStuff[menubar]);      // actually displays the gate
 
         gfxFlushBuffers();
         gfxSwapBuffers();
