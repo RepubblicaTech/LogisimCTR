dd5b5d6baba054ca61402ece695b81ef768f4895
RepubblicaTech <76620155+RepubblicaTech@users.noreply.github.com>
2023-09-01 12:49:57 +0200

    Update lib.h

--- source/include/lib.h
+++ source/include/lib.h
@@ -1,12 +1,11 @@
 struct logicGate
 {
     int inputs;
+    int outputs;
 
     int outType;
     /*
         0 = 1 bit (0/1)
         1 = custom (2 bits, 1 byte etc..)
     */
-
-    int outputs;
 };
